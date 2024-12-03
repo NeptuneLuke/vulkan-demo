e64ea66a19718f578fc76f30f0e689a84f7e2978
NeptuneLuke <luca1cortinovis@gmail.com>
2024-12-03 13:34:00 +0100

    Setup enviroment

--- main.cpp
+++ main.cpp
@@ -1,37 +1,48 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include <vulkan/vulkan.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib> // EXIT_FAILURE | EXIT_SUCCESS
+
+
+class VulkanDemo {
+
+public:
+	
+	void run() {
+		init_vulkan();
+		main_loop();
+		cleanup();
+	}
+
+private:
+
+	void init_vulkan() {
+	
+	}
+
+	void main_loop() {
+	
+	}
+
+	void cleanup() {
+	
+	}
+
+};
 
 
 int main() {
 	
-	glfwInit();
-	
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow * window = glfwCreateWindow(800, 600, "Vulkan demo", nullptr, nullptr);
-	
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	VulkanDemo demo;
 
-    std::cout << extensionCount << " extensions supported \n";
-	
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-	
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+	try {
+		demo.run();
 	}
-	
-    glfwDestroyWindow(window);
-	
-	glfwTerminate();
-	
-	return 0;
+	catch (const std::exception &ex) {
+		std::cerr << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
