b8a65b1d5349ab95ca955cd9023b516485f61b7f
NeptuneLuke <luca1cortinovis@gmail.com>
2024-12-09 16:21:49 +0100

    Removed vk_renderpass.cpp/.hpp

---
+++ vk_graphics_pipeline.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "my_utils.hpp"
+
+
+void create_render_pass(VkRenderPass& vk_render_pass, VkDevice vk_logic_device, VkFormat& vk_swapchain_image_format);
--- vk_renderpass.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "my_utils.hpp"
-
-
-void create_render_pass(VkRenderPass& vk_render_pass, VkDevice vk_logic_device, VkFormat& vk_swapchain_image_format);
